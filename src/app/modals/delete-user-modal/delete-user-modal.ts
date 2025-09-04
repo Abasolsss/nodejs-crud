3d394842c4cbc693953d3e9bbbe8bc1b892c98de
Abasoooooool <ramzeeesss666@gmail.com>
2025-09-04 15:47:09 +0800

    first commit

---
+++ src/app/modals/delete-user-modal/delete-user-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-user-modal',
+  imports: [],
+  templateUrl: './delete-user-modal.html',
+  styleUrl: './delete-user-modal.scss'
+})
+export class DeleteUserModal {
+
+}
