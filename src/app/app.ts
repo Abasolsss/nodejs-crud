97833397c613019f9db9952071fe47c96b75977d
Abasoooooool <ramzeeesss666@gmail.com>
2025-09-04 18:24:57 +0800

    modal added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
-
+// import { AddUserModal } from './modals/add-user-modal/add-user-modal';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,Navbar],
--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { AddUserModal } from '../../modals/add-user-modal/add-user-modal';
+import {MatDialog} from '@angular/material/dialog'
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AddUserModal],
   templateUrl: './navbar.html',
   styleUrl: 'navbar.scss'
 })
+
+
 export class Navbar {
+
+constructor(private dialog: MatDialog) {}
+
+
+  testFunction () {
+    this.dialog.open(AddUserModal, {
+       
+    })
+  }
 }
+
